d37cb8505009b7119cb48727d5803d641e75599a
henryshiu <henryshiu@gamania.com>
2022-11-29 10:32:46 +0800

    Complete LeetCode #1710

--- src/leetcode/editor/en/P1710MaximumUnitsOnATruck.java
+++ src/leetcode/editor/en/P1710MaximumUnitsOnATruck.java
@@ -1,5 +1,5 @@
-//You are assigned to put some amount of boxes onto one truck. You are given a 2
-//D array boxTypes, where boxTypes[i] = [numberOfBoxesi, numberOfUnitsPerBoxi]: 
+//You are assigned to put some amount of boxes onto one truck. You are given a 2D array boxTypes,
+// where boxTypes[i] = [numberOfBoxesi, numberOfUnitsPerBoxi]:
 //
 // numberOfBoxesi is the number of boxes of type i. 
 // numberOfUnitsPerBoxi is the number of units in each box of the type i. 
@@ -33,19 +33,34 @@
 // 👍 3228 👎 182
 
 package leetcode.editor.en;
+
+import java.util.Arrays;
+
 //Java：Maximum Units on a Truck
 public class P1710MaximumUnitsOnATruck{
     public static void main(String[] args) {
         Solution solution = new P1710MaximumUnitsOnATruck().new Solution();
+        System.out.println(solution.maximumUnits(new int[][]{new int[]{1,3}, new int[]{2,2},
+                        new int[]{3,1}}, 4));
         System.out.println(solution.maximumUnits(new int[][]{new int[]{5,10}, new int[]{2,5},
-                new int[]{4,7}, new int[]{3,9}},
-            4));
+                new int[]{4,7}, new int[]{3,9}}, 10));
     }
     //leetcode submit region begin(Prohibit modification and deletion)
 
     class Solution {
         public int maximumUnits(int[][] boxTypes, int truckSize) {
-            return -1;
+            Arrays.sort(boxTypes, (a, b) -> b[1] - a[1]);
+            int totalUnit = 0;
+            for(int[] boxType : boxTypes){
+                Integer amount = boxType[0];
+                Integer unit = boxType[1];
+                if(amount >= truckSize){
+                    return totalUnit + (truckSize * unit);
+                }
+                truckSize = truckSize - amount;
+                totalUnit = totalUnit + (amount * unit);
+            }
+            return totalUnit;
         }
     }
 }
