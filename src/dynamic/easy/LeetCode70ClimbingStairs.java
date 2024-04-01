51a7dceb763e8a9743a42770aa988d77cb57a38e
HenryShiu <jshiu@opentable.com>
2024-04-01 16:35:17 +0800

    New solution for leetcode 70

--- src/dynamic/easy/LeetCode70ClimbingStairs.java
+++ src/dynamic/easy/LeetCode70ClimbingStairs.java
@@ -3,14 +3,17 @@ package dynamic.easy;
 public class LeetCode70ClimbingStairs {
 
     public int climbStairs(int n) {
-        if(n==0 || n==1) return n;
-        int[] array = new int[n+1];
-        array[0] = 0;
-        array[1] = 1;
-        for(int i=2; i <=n; i++){
-            array[i] = array[i-1] + array[i-2];
+        if(n <=2){
+            return n;
         }
-        return array[n] + array[n-1];
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 
     public static void main(String[] args){
