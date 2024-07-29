b66332b10e5282389be0eb19b7ae8487cea71ea6
HenryShiu <jshiu@opentable.com>
2024-07-29 12:06:12 +0800

    Add more solution

--- src/backtracking/medium/Combinations.java
+++ src/backtracking/medium/Combinations.java
@@ -2,6 +2,7 @@ package backtracking.medium;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Given two integers n and k, return all possible combinations of k numbers chosen from the range [1, n].
@@ -12,7 +13,24 @@ import java.util.List;
  */
 public class Combinations {
 
-    List<List<Integer>> result = new LinkedList<>();
+    public List<List<Integer>> combine2(int n, int k) {
+        List<List<Integer>> result = new LinkedList<>();
+        Stack<Integer> path = new Stack<>(); //已經做出的選擇
+        backtrack2(path, 1, n, k, result);
+        return result;
+    }
+
+    private void backtrack2(Stack<Integer> path, int start, int n, int k, List<List<Integer>> result) {
+        if(path.size() == k){//終止條件, 選擇是否符合要求
+            result.add(new LinkedList<>(path));
+            return;
+        }
+        for(int i=start; i <= n; i++){
+            path.add(i);//選擇
+            backtrack2(path, i+1, n, k, result);
+            path.pop();//清空,從選擇清單做下一個選擇
+        }
+    }
 
     /**
      *
@@ -21,19 +39,20 @@ public class Combinations {
      * @return
      */
     public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new LinkedList<>();
         LinkedList<Integer> path = new LinkedList<>(); //已經做出的選擇
-        backtrack(path, 1, n, k);
+        backtrack(path, 1, n, k, result);
         return result;
     }
 
-    private void backtrack(LinkedList<Integer> path, int start, int n, int k) {
+    private void backtrack(LinkedList<Integer> path, int start, int n, int k, List<List<Integer>> result) {
         if(path.size() == k){//終止條件, 選擇是否符合要求
             result.add(new LinkedList<>(path));
             return;
         }
         for(int i=start; i <= n; i++){
             path.add(i);//選擇
-            backtrack(path, i+1, n, k);
+            backtrack(path, i+1, n, k, result);
             path.removeLast();//清空,從選擇清單做下一個選擇
         }
     }
@@ -41,5 +60,6 @@ public class Combinations {
     public static void main(String args[]){
         Combinations c = new Combinations();
         System.out.println(c.combine(4,2));
+        System.out.println(c.combine2(4,2));
     }
 }
